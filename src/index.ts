09696702a18c2a0cd153a58d1a61cbc8f223f85d
Ryan Wu <imgreenwu@gmail.com>
2020-12-15 00:57:14 +0800

    update global option

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,18 @@ import AOS from './AOS.vue'
 import { App } from 'vue'
 import AOSAnchor from './AOSAnchor'
 
-// let aosOption: AosOptions
-
-// export const mixin = {
-
-//     async beforeMount() {
-
-//         const aos = await import('aos')
-
-//         aos.init(aosOption)
-
-//         Vue.prototype.$aos = aos
-
-//     }
-
-// }
-
 export default {
 
     async install(app: App, options: AosOptions) {
 
         app.component('AOS', AOS)
         app.directive('aos-anchor', AOSAnchor)
-
-        // app.directive('aos-anchor', AOSAnchor)
-
-        // const aos = await import('aos')
-
         aos.init(options)
 
-        app.config.globalProperties.$aos = aos
+        app.config.globalProperties.$aos = {
+            ...aos,
+            options
+        }
 
     }
 
